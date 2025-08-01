8182539e967a2971aaf58d2b31635fc2c76438ab
aerusinfotech <aerusinfotech@gmail.com>
2025-08-01 18:46:34 +0530

    Converted from Class Component to Functional Component

--- src/components/board-admin.component.js
+++ src/components/board-admin.component.js
@@ -1,48 +1,37 @@
-import React, { Component } from "react";
-
+import React, { useState, useEffect } from "react";
 import UserService from "../services/user.service";
 import EventBus from "../common/EventBus";
 
-export default class BoardAdmin extends Component {
-  constructor(props) {
-    super(props);
+export default function BoardAdmin() {
+  const [content, setContent] = useState("");
 
-    this.state = {
-      content: ""
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     UserService.getAdminBoard().then(
-      response => {
-        this.setState({
-          content: response.data
-        });
+      (response) => {
+        setContent(response.data);
       },
-      error => {
-        this.setState({
-          content:
-            (error.response &&
-              error.response.data &&
-              error.response.data.message) ||
-            error.message ||
-            error.toString()
-        });
+      (error) => {
+        const errorMessage =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+
+        setContent(errorMessage);
 
         if (error.response && error.response.status === 401) {
           EventBus.dispatch("logout");
         }
       }
     );
-  }
+  }, []); // empty dependency array ensures this runs once like componentDidMount
 
-  render() {
-    return (
-      <div className="container">
-        <header className="jumbotron">
-          <h3>{this.state.content}</h3>
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="container">
+      <header className="jumbotron">
+        <h3>{content}</h3>
+      </header>
+    </div>
+  );
 }
